4d3807f0a8cb7f85ec9a5c223c0368a5d88630c3
whysixmift <miftasigma11@gmail.com>
2026-09-23 17:23:14 +0700

    feat: add donate button

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,4 +1,22 @@
-<header style="margin-bottom: 24px;">
-	<h1 class="brand">Morrow</h1>
-	<p class="subbrand">Download media from YouTube.</p>
+<header class="header">
+	<div>
+		<h1 class="brand">Morrow</h1>
+		<p class="subbrand">Download media from YouTube.</p>
+	</div>
+	<a
+		href="https://saweria.co/avrjulian"
+		target="_blank"
+		rel="noopener noreferrer"
+		class="btn-coffee"
+		title="Support Morrow on Saweria"
+	>
+		<svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+			<path d="M18 8h1a4 4 0 0 1 0 8h-1"></path>
+			<path d="M2 8h16v9a4 4 0 0 1-4 4H6a4 4 0 0 1-4-4V8z"></path>
+			<line x1="6" y1="1" x2="6" y2="4"></line>
+			<line x1="10" y1="1" x2="10" y2="4"></line>
+			<line x1="14" y1="1" x2="14" y2="4"></line>
+		</svg>
+		<span>Buy me a coffee</span>
+	</a>
 </header>
